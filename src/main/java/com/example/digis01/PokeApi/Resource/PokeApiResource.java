7f73f3d9d15dbc763bb611f91436cb780642322c
digis <digis@ALIEN23>
2025-06-06 12:25:08 -0600

    Security

---
+++ src/main/java/com/example/digis01/PokeApi/Resource/PokeApiResource.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.example.digis01.PokeApi.Resource;
+
+/**
+ *
+ * @author digis
+ */
+public interface PokeApiResource {
+    
+}
